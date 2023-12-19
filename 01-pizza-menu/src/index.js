e10ee69b03f4a7c9673b59212c11e64dc85c5268
geancanova <geancanovawt@gmail.com>
2023-12-19 13:59:31 -0300

    fix: conditional class

--- 01-pizza-menu/src/index.js
+++ 01-pizza-menu/src/index.js
@@ -95,7 +95,7 @@ function Menu() {
 
 function Pizza({ pizzaObj }) {
   return (
-    <li className={`pizza ${pizzaObj.soldOut && "sold-out"}`}>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
