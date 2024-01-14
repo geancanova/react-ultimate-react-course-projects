af6b3acc38dab2bc4b7e2b51b09a9028b73ae512
geancanova <geancanovawt@gmail.com>
2024-01-14 17:01:27 -0300

    add: create component to render country image flags

---
+++ 10-worldwise/src/components/EmojiFlagImage.jsx
@@ -0,0 +1,10 @@
+function EmojiFlagImage({ emoji }) {
+  const countryCode = Array.from(emoji, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join("");
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+}
+
+export default EmojiFlagImage;
