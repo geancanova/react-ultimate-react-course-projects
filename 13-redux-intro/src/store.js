7d769bd277f591f10c5ca4cfdd7299037cadb7d1
geancanova <geancanovawt@gmail.com>
2024-01-22 10:22:06 -0300

    add: modern way of use Redux

--- 13-redux-intro/src/store.js
+++ 13-redux-intro/src/store.js
@@ -1,13 +1,10 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customer/customerSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: { account: accountReducer, customer: customerReducer },
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
