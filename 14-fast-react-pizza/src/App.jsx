14cf9c4df58c0967f406666a2e17630f30022d1f
geancanova <geancanovawt@gmail.com>
2024-01-23 14:55:29 -0300

    add: handling errors with error elements

--- 14-fast-react-pizza/src/App.jsx
+++ 14-fast-react-pizza/src/App.jsx
@@ -4,12 +4,14 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
+import Error from "./ui/Error";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -19,6 +21,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
