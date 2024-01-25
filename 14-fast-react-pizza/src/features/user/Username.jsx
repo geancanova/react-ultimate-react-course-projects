46a75e79529d3570a90879bae306e30234fde1e3
geancanova <geancanovawt@gmail.com>
2024-01-25 15:56:24 -0300

    add: create userSlice, store and Provider

--- 14-fast-react-pizza/src/features/user/Username.jsx
+++ 14-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Canova</div>;
+  const username = useSelector((state) => state.user.usermame);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
