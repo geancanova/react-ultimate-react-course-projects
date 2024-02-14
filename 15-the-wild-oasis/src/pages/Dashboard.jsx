4ff812dede9462993e1220c0d5477e272f20e442
geancanova <geancanovawt@gmail.com>
2024-02-14 11:14:01 -0300

    add: create hooks to get recent bookings and recent stays

--- 15-the-wild-oasis/src/pages/Dashboard.jsx
+++ 15-the-wild-oasis/src/pages/Dashboard.jsx
@@ -1,9 +1,18 @@
 import DashboardFilter from "../features/dashboard/DashboardFilter";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
+import { useRecentBookings } from "../features/dashboard/useRecentBookings";
+import { useRecentStays } from "../features/dashboard/useRecentStays";
 import Heading from "../ui/Heading";
+import Spinner from "../ui/Spinner";
 import Row from "../ui/Row";
 
 function Dashboard() {
+  const { bookings, isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading2 } = useRecentStays();
+
+  console.log(bookings, confirmedStays, stays);
+
+  if (isLoading1 || isLoading2) return <Spinner />;
   return (
     <>
       <Row type="horizontal">
