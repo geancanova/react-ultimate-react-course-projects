7a2d0d1d7cbc21f2eb78872a3dcc5430c6d950f9
geancanova <geancanovawt@gmail.com>
2024-02-15 09:46:16 -0300

    feat: use user prefered color scheme as the default theme

--- 15-the-wild-oasis/src/context/DarkModeContext.jsx
+++ 15-the-wild-oasis/src/context/DarkModeContext.jsx
@@ -4,7 +4,13 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  const userColorScheme = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    userColorScheme,
+    "isDarkMode"
+  );
 
   useEffect(
     function () {
