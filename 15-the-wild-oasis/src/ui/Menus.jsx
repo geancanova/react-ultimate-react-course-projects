91de4439bf822629ea869ccfce4912ecc23f1976
geancanova <geancanovawt@gmail.com>
2024-02-15 09:35:28 -0300

    fix: menu bug

--- 15-the-wild-oasis/src/ui/Menus.jsx
+++ 15-the-wild-oasis/src/ui/Menus.jsx
@@ -86,6 +86,8 @@ function Toggle({ id }) {
   const { openId, open, close, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
+
     openId === "" || openId !== id ? open(id) : close();
     const rect = e.target.closest("button").getBoundingClientRect();
 
@@ -104,7 +106,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
 
