b58a9c5dbcfedcec2f28c02616d805183d38d1de
geancanova <geancanovawt@gmail.com>
2024-07-01 16:12:21 -0300

    add: cabins filter

--- 21-the-wild-oasis-website/app/_components/CabinList.js
+++ 21-the-wild-oasis-website/app/_components/CabinList.js
@@ -2,16 +2,39 @@ import CabinCard from "@/app/_components/CabinCard"
 import { getCabins } from "@/app/_lib/data-service";
 import { unstable_noStore as noStore } from "next/cache";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   noStore();
 
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
 
+  let displayedCabins;
+
+  switch (filter) {
+    case 'small':
+      displayedCabins = cabins.filter((cabin) => {
+        return cabin.maxCapacity <= 3
+      })
+      break;
+    case 'medium':
+      displayedCabins = cabins.filter((cabin) => {
+        return cabin.maxCapacity > 3 && cabin.maxCapacity < 8
+      })
+      break;
+    case 'large':
+      displayedCabins = cabins.filter((cabin) => {
+        return cabin.maxCapacity >= 8
+      })
+      break;
+    default:
+      displayedCabins = cabins;
+      break;
+  }
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
