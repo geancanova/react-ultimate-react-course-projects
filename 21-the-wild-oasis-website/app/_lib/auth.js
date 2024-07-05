bdbf3e97999ab8cd069b10b165a015ec5158a3f5
geancanova <geancanovawt@gmail.com>
2024-07-05 11:32:14 -0300

    add: create new guest at guests table on first sign in

--- 21-the-wild-oasis-website/app/_lib/auth.js
+++ 21-the-wild-oasis-website/app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -11,7 +12,29 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user; // !! converts to a boolean value
-    }
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) {
+          await createGuest({
+            fullName: user.name,
+            email: user.email,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+
+      return session;
+    },
   },
   pages: {
     signIn: "/login"
