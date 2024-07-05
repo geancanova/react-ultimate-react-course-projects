bd7d29d4c1531a3db7fea02aa4f4952b553b3e42
geancanova <geancanovawt@gmail.com>
2024-07-05 14:22:22 -0300

    add: manual cache revalidation and new useFormStatus hook to the update guest form

--- 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -1,6 +1,8 @@
 'use client'
 
+import { useFormStatus } from 'react-dom'
 import { updateGuest } from "@/app/_lib/actions"
+import Image from 'next/image'
 
 function UpdateProfileForm({ guest, children }) {
   const { fullName, email, nationalID, countryFlag } = guest
@@ -31,8 +33,10 @@ function UpdateProfileForm({ guest, children }) {
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
           {countryFlag && (
-            <img
+            <Image
               src={countryFlag}
+              width={30}
+              height={20}
               alt="Country flag"
               className="h-5 rounded-sm"
             />
@@ -52,12 +56,22 @@ function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
   )
 }
+
+function Button() {
+  const { pending } = useFormStatus();
+
+  return <button
+    disabled={pending}
+    className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+  >
+    {pending ? 'Updating...' : 'Update profile'}
+  </button>
+}
+
 
 export default UpdateProfileForm
