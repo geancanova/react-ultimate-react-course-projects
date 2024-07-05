2f3a1114f8264ec3d676ea3f02133169c2ac462a
geancanova <geancanovawt@gmail.com>
2024-07-05 14:09:11 -0300

    add: update the guest using a Server Action

--- 21-the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/app/_lib/actions.js
@@ -1,6 +1,31 @@
 'use server';
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+
+export async function updateGuest(formData) {
+  const session = await auth();
+
+  if (!session) throw new Error('You must be signed in!');
+
+  const nationalID = formData.get('nationalID');
+  const [nationality, countryFlag] = formData.get('nationality').split('%');
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) throw new Error('Please provide a valid national ID');
+
+  const updateData = {
+    nationalID,
+    nationality,
+    countryFlag
+  };
+
+  const { data, error } = await supabase
+    .from('guests')
+    .update(updateData)
+    .eq('id', session.user.guestId);
+
+  if (error) throw new Error('Guest could not be updated');
+}
 
 export async function signInAction() {
   await signIn('google', {
