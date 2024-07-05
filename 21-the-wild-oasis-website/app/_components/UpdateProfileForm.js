2fbe39c226642d25f00c78d0b1b2463da537eeb3
geancanova <geancanovawt@gmail.com>
2024-07-05 19:09:43 -0300

    add: update reservation functionality and create a SubmitButton reusable component

--- 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useFormStatus } from 'react-dom'
+import SubmitButton from '@/app/_components/SubmitButton'
 import { updateGuest } from "@/app/_lib/actions"
 import Image from 'next/image'
 
@@ -56,22 +56,10 @@ function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingText="Updating...">Update profile</SubmitButton>
       </div>
     </form>
   )
 }
-
-function Button() {
-  const { pending } = useFormStatus();
-
-  return <button
-    disabled={pending}
-    className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-  >
-    {pending ? 'Updating...' : 'Update profile'}
-  </button>
-}
-
 
 export default UpdateProfileForm
